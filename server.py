09705773e25d5e3a62afb401420c4410d77b835c
Matt Ellen <matt@matthewellen.co.uk>
2014-04-17 19:03:51 +0100

    Initial Commit

---
+++ server.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+import cherrypy
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+class Server(object):
+
+    @cherrypy.expose
+    def index(self):
+        return 'hello world'
+
+conf = {
+    'global': {
+        'server.socket_host': '0.0.0.0',
+        'server.socket_port': 8080,
+        },
+    '/js':{
+        'tools.staticdir.on': True,
+        'tools.staticdir.dir': os.path.join(current_dir, 'js'),
+        },
+    '/css':{
+        'tools.staticdir.on': True,
+        'tools.staticdir.dir': os.path.join(current_dir, 'css'),
+        },
+    '/img':{
+        'tools.staticdir.on': True,
+        'tools.staticdir.dir': os.path.join(current_dir, 'img'),
+        }
+}
+
+cherrypy.server.socket_host = '0.0.0.0'
+cherrypy.quickstart(Server(), '/', conf)
